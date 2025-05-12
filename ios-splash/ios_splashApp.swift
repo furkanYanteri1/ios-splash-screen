49d3c3bcf57e69bbc5d7b213a11a9e8299249d62
Furkan Yanteri <furkanyanteri@gmail.com>
2025-05-12 18:55:42 +0300

    Initial Commit

---
+++ ios-splash/ios_splashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_splashApp.swift
+//  ios-splash
+//
+//  Created by Furkan Yanteri on 12.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ios_splashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
